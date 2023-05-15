b07165be75ea7e146b46eaa95516d71309b55f13
michellenikeetha <88478424+michellenikeetha@users.noreply.github.com>
2023-05-15 15:04:26 +0530

    Add files via upload

---
+++ include/emsidebar.php
@@ -0,0 +1,17 @@
+<div class="logo">
+        <span class="logo-img"><img src="../images/logo.png" alt="logo"></span>
+    </div>
+
+    <div class="nav-links">
+        <a href="emdashboard.php"><i class="fa fa-th-large" ></i><span class="links_name">Dashboard</span></a>
+        <a href="emorders.php"><i class="fa fa-list-ul" ></i><span class="links_name">Orders</span></a>
+        <a href="emrecords.php"><i class="fa fa-stack-exchange" ></i><span class="links_name">Records</span></a>
+        <a href="emchangepassword.php"><i class="fa fa-key" ></i><span class="links_name">Privacy</span></a>
+
+        <div class="log-out">
+            <a href="../logout.php"><i class="fa fa-sign-out" ></i><span class="links_name">Log out</span></a>
+        </div>
+        
+    </div>
+
+
