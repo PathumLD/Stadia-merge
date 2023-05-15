0c7556f7160fc6e6907a0051839cb43cce3d0071
chamudisandunika <88612114+chamudisandunika@users.noreply.github.com>
2023-05-15 15:19:22 +0530

    Add files via upload

--- Manager/managerbookingsbadminton2.php
+++ Manager/managerbookingsbadminton2.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/calendar.css">
+    <link rel="stylesheet" href="../css/slots.css">
  
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
